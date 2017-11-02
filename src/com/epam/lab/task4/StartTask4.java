e0361c4382cc51b80e35ad8eb12e39185f8d44c4
OlgaKhytriak <KhytriakOlga@gmail.com>
2017-11-02 13:36:26 +0200

    Start

---
+++ src/com/epam/lab/task4/StartTask4.java
@@ -0,0 +1,21 @@
+package com.epam.lab.task4;
+
+import org.apache.log4j.Logger;
+
+public class StartTask4 {
+	private static final Logger LOG = Logger.getLogger(StartTask4.class);
+
+	public static void main(String[] args) {
+		StartTask4 startTask4 = new StartTask4();
+		startTask4.test();	
+		LOG.info("The End of StartTask4 ++  ----------------------");
+
+	}
+
+	private void test() {
+		
+
+	}
+
+
+}
